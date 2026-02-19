c50fbd9769e777ffe537f697e54ccb0fbc938b6c
Wojciech Maciejewski <wojmak2000@gmail.com>
2026-02-19 16:04:35 +0100

    gitignore

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-console.log("cwel")
-while(true){
-    
+import express from "express"
+const app = express()
+const port = 3005
+
+app.get('/', (req, res)) => {
+    res.send('hello world')
+
 }
+app.listen(port,()=>{
+    console.log('http://localhost:${port} on port ${port}');
+});
